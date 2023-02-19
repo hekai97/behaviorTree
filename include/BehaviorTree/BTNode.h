0c4f39cbe28931046d59e239245ca4bf0b8c73f3
hekai97 <hekaigs@gmail.com>
2023-02-19 17:55:54 +0800

    添加了基本类型

--- include/BehaviorTree/BTNode.h
+++ include/BehaviorTree/BTNode.h
@@ -1,5 +1,20 @@
 # pragma once
 
+enum class BTResult{
+    SUCCESS,
+    FAILURE,
+    RUNNING
+};
+
+enum class BTNodeType{
+    COMPOSITE,
+    DECORATOR,
+    ACTION,
+    CONDITION
+};
+
+
+
 class BTNode
 {
 private:
@@ -7,6 +22,7 @@ private:
 public:
     BTNode(/* args */);
     ~BTNode();
+    virtual BTNodeType getType() const = 0;
 };
 
 BTNode::BTNode(/* args */)
